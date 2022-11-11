5f9787dd400d26be44371399a7dcbcbe6d35aadd
lamba <programmer.lamba@gmail.com>
2022-11-11 22:54:18 +0200

    added basic header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {APP_BASE_HREF} from "@angular/common";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/projects/enlightenment'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
